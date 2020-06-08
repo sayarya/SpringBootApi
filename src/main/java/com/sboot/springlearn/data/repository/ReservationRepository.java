62b9b7d4a1208a1f655c28604a2525876b5801ca
sayarya <64267777+sayarya@users.noreply.github.com>
2020-06-08 12:10:41 +0530

    Add files via upload
    Basic Spring boot application :
    1. Thymeleaf for creating view
    2. H2 Database

---
+++ src/main/java/com/sboot/springlearn/data/repository/ReservationRepository.java
@@ -0,0 +1,13 @@
+package com.sboot.springlearn.data.repository;
+
+import com.sboot.springlearn.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+
+    Iterable<Reservation> findReservationByReservationDate(Date date);
+}
